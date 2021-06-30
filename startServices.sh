b13af9181a0432ca22ea0cd739c353b04f1f3124
Fernando Dumont <fernando@fernando.com.br>
2021-06-30 10:16:13 -0300

    Split services

---
+++ startServices.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+current_dir=$(pwd)
+script_dir=$(dirname $0)
+
+if [ $script_dir = '.' ]
+then
+  script_dir="$current_dir"
+fi
+
+docker-compose -f $script_dir/docker-compose-services.yml up -d 
+
